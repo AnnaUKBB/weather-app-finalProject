de79fd2fa6b55b321f08e307217b00ffac580b9e
AnnaUKBB <aniutaloboda@gmail.com>
2022-10-06 21:11:48 +0200

    Showing condition of weather (added api)

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#windspeed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "25f839ad37bb008537bd01807a41779a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
